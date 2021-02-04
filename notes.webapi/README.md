7f3125650dc94d352782ae650ffb53e87c9b94e3
rafamoliv <rafaoliv.ti@gmail.com>
2021-02-04 13:17:51 -0300

    commit project

---
+++ notes.webapi/README.md
@@ -0,0 +1,3 @@
+# notes.webapi
+
+This project has a tutorial on: https://www.youtube.com/watch?v=JW44lrsCkUM&feature=youtu.be&ab_channel=AsielAlvarez
---
+++ src/services/notes.js
@@ -0,0 +1,51 @@
+import { ActionCreators } from "../redux/notesReducer";
+import * as axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://localhost:5001/notes",
+});
+
+export const GetNotes = async (dispatch) => {
+  try {
+    //api call
+
+    const { data } = await axiosInstance.get();
+
+    dispatch(ActionCreators.setNotes(data));
+  } catch {
+    console.log("Error");
+  }
+};
+
+export const DeleteNote = async (dispatch, note) => {
+  try {
+    //api call
+    await axiosInstance.delete(`/${note.id}`);
+
+    dispatch(ActionCreators.deleteNote(note));
+  } catch {
+    console.log("Error");
+  }
+};
+
+export const NewNote = async (dispatch, note) => {
+  try {
+    // api call
+    const { data } = await axiosInstance.post("", note);
+
+    dispatch(ActionCreators.newNote(data));
+  } catch {
+    console.log("Error!");
+  }
+};
+
+export const EditNote = async (dispatch, note) => {
+  try {
+    //api call
+    await axiosInstance.put("", note);
+
+    dispatch(ActionCreators.editNote(note));
+  } catch {
+    console.log("Error");
+  }
+};
